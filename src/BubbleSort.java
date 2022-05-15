fa5f2e411624b516a74fb1e2c3c0cbb4d61ddd36
Sirin Shaikh <97180903+shaikhsirin@users.noreply.github.com>
2022-05-15 16:59:27 +0530

    Added:-  Print the Array for Sorting Algorithm

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -10,6 +10,14 @@ class BubbleSort {
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
+         /* Prints the array */
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
     }
 
 }
