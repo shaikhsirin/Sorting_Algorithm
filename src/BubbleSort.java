534f09c5cb02ef7eb47475cce0ab0f642f823477
Sirin Shaikh <97180903+shaikhsirin@users.noreply.github.com>
2022-05-15 17:02:54 +0530

    Added:- Driver method to testing the Array for Sorting Algorithm

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,6 +1,7 @@
 //  program for implementation of Bubble Sort
 class BubbleSort {
-    void bubbleSort(int arr[]) {
+    void bubbleSort(int arr[])
+    {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++)
             for (int j = 0; j < n - i - 1; j++)
@@ -10,7 +11,9 @@ class BubbleSort {
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
-         /* Prints the array */
+    }
+
+    /* Prints the array */
     void printArray(int arr[])
     {
         int n = arr.length;
@@ -18,6 +21,13 @@ class BubbleSort {
             System.out.print(arr[i] + " ");
         System.out.println();
     }
-    }
 
-}
+    // Driver method to test above
+    public static void main(String args[])
+    {
+        BubbleSort ob = new BubbleSort();
+        int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
