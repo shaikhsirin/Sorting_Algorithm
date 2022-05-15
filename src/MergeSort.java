fa0dba00d58f30f3ecafa48b7d1af9a3a0be6507
Sirin Shaikh <97180903+shaikhsirin@users.noreply.github.com>
2022-05-15 17:16:08 +0530

    Added:- Initial indexes of first and second subarrays

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -19,6 +19,11 @@ class MergeSort
             L[i] = arr[l + i];
         for (int j = 0; j < n2; ++j)
             R[j] = arr[m + 1 + j];
+        
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
 
         
 }
